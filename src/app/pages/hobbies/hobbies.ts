3ef603303e155e205534c19a2a578a9b4f22d635
St487 <siewting8008@gmail.com>
2025-11-25 15:11:06 +0800

    Initial Angular resume site

---
+++ src/app/pages/hobbies/hobbies.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-hobbies',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './hobbies.html',
+  styleUrl: './hobbies.css',
+})
+export class Hobbies {
+
+}
